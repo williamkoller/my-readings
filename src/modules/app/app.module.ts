4a9daa6204025cc195bf4a8766d7e17ed77a864d
William Koller <williamkoller404@gmail.com>
2022-04-06 22:04:22 -0300

    refactor: organize imports

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,29 +1,8 @@
-import { forwardRef, Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import envFolderPath, { environments } from '@/config/environments';
-import { MongooseModule } from '@nestjs/mongoose';
-import { BooksModule } from '@/modules/books/books.module';
-import { MulterModule } from '@nestjs/platform-express';
+import { Module } from '@nestjs/common';
+import { imports } from '@/modules/app/imports';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: envFolderPath.folderPath,
-      load: [environments],
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        uri: config.get('mongoUri'),
-      }),
-    }),
-    MulterModule.register({
-      dest: './files',
-    }),
-    forwardRef(() => BooksModule),
-  ],
+  imports,
   controllers: [],
   providers: [],
 })
---
+++ src/modules/app/imports/index.ts
@@ -0,0 +1,21 @@
+import envFolderPath, { environments } from '@/config/environments';
+import { BooksModule } from '@/modules/books/books.module';
+import { forwardRef } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+export const imports = [
+  ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: envFolderPath.folderPath,
+    load: [environments],
+  }),
+  MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (config: ConfigService) => ({
+      uri: config.get('mongoUri'),
+    }),
+  }),
+  forwardRef(() => BooksModule),
+];
