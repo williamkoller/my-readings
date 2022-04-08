1c151148fd41a9c9c6700082913862509b843347
William Koller <williamkoller404@gmail.com>
2022-04-07 21:32:55 -0300

    feat: add UsersController with methods store, show and index

---
+++ src/modules/users/controllers/users.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AddUserDto } from '@/modules/users/dtos/add-user.dto';
+import { User } from '@/modules/users/schemas/user.schema';
+import { AddUserService } from '@/modules/users/services/add-user/add-user.service';
+import { FindAllUsersService } from '../services/find-all-users/find-all-users.service';
+import { FindUserByIdService } from '../services/find-user-by-id/find-user-by-id.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(
+    private readonly addUSerService: AddUserService,
+    private readonly findAllUsersService: FindAllUsersService,
+    private readonly findUserByIdService: FindUserByIdService,
+  ) {}
+
+  @Post()
+  async store(@Body() addUserDto: AddUserDto): Promise<User> {
+    return await this.addUSerService.newUser(addUserDto);
+  }
+
+  @Get()
+  async index(): Promise<User[]> {
+    return await this.findAllUsersService.findAll();
+  }
+
+  @Get(':_id')
+  async show(@Param('_id') _id: string): Promise<User> {
+    return await this.findUserByIdService.findUserById(_id);
+  }
+}
