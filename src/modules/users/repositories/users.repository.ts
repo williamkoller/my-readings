d54cd2e4f8ef50eed648b490c550976c197b3ea9
William Koller <williamkoller404@gmail.com>
2022-04-07 21:33:49 -0300

    feat: add UsersRepository

---
+++ src/modules/users/repositories/users.repository.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '@/modules/users/schemas/user.schema';
+import { Model } from 'mongoose';
+import { propertyFalseMongo } from '@/utils/property-false-mongo';
+import { AddUserDto } from '@/modules/users/dtos/add-user.dto';
+
+@Injectable()
+export class UsersRepository {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async add(addUserDto: AddUserDto): Promise<User> {
+    const userCreated = new this.userModel(addUserDto, propertyFalseMongo);
+    return userCreated.save();
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return await this.userModel.findOne({ email }, propertyFalseMongo);
+  }
+
+  async findById(_id: string): Promise<User> {
+    return await this.userModel.findOne({ _id }, propertyFalseMongo);
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.userModel
+      .find({}, propertyFalseMongo)
+      .select('-password');
+  }
+}
