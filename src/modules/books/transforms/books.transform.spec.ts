eaac116b52daa2504b83e0ff85064c6090baaa73
Luan Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-04-18 10:48:12 -0300

    refact: update the variable name

--- src/modules/books/transforms/books.transform.spec.ts
+++ src/modules/books/transforms/books.transform.spec.ts
@@ -1,18 +1,22 @@
 import { Book } from '@/modules/books/schemas/book.schema';
 import { booksTransform, bookTransform } from './books.transform';
 
+const makeBook = (): Book => {
+  return {
+    _id: 'any_id',
+    name: 'any_name',
+    description: 'any_description',
+    author: 'any_author',
+    status: 'any_status',
+    url: 'any_url',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+};
+
 describe('booksTransform', () => {
   test('should transform book to output', () => {
-    const book: Book = {
-      _id: 'any_id',
-      name: 'any_name',
-      description: 'any_description',
-      author: 'any_author',
-      status: 'any_status',
-      url: 'any_url',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    const book: Book = makeBook();
 
     const actual = bookTransform(book);
 
@@ -27,27 +31,18 @@ describe('booksTransform', () => {
   });
 
   test('should transform books to output', () => {
-    const book: Book = {
-      _id: 'any_id',
-      name: 'any_name',
-      description: 'any_description',
-      author: 'any_author',
-      status: 'any_status',
-      url: 'any_url',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    const books: Book[] = [makeBook()];
 
-    const actual = booksTransform([book]);
+    const actual = booksTransform(books);
 
     expect(actual.length).toBe(1);
-    expect(actual[0].id).toBe(book._id);
-    expect(actual[0].name).toBe(book.name);
-    expect(actual[0].description).toBe(book.description);
-    expect(actual[0].author).toBe(book.author);
-    expect(actual[0].status).toBe(book.status);
-    expect(actual[0].url).toBe(book.url);
-    expect(actual[0].createdAt).toStrictEqual(book.createdAt);
-    expect(actual[0].updatedAt).toStrictEqual(book.updatedAt);
+    expect(actual[0].id).toBe(books[0]._id);
+    expect(actual[0].name).toBe(books[0].name);
+    expect(actual[0].description).toBe(books[0].description);
+    expect(actual[0].author).toBe(books[0].author);
+    expect(actual[0].status).toBe(books[0].status);
+    expect(actual[0].url).toBe(books[0].url);
+    expect(actual[0].createdAt).toStrictEqual(books[0].createdAt);
+    expect(actual[0].updatedAt).toStrictEqual(books[0].updatedAt);
   });
 });
