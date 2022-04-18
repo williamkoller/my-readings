941cc6ea0c223d4855e2c3a94f13bbec0f6f8933
Luan Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-04-18 10:40:50 -0300

    refact: remove code duplicated

--- src/modules/books/transforms/books.transform.ts
+++ src/modules/books/transforms/books.transform.ts
@@ -2,16 +2,7 @@ import { Book } from '@/modules/books/schemas/book.schema';
 import { BookOutputType } from '@/modules/books/types/book-output.type';
 
 export const booksTransform = (books: Book[]): BookOutputType[] => {
-  return books.map((book) => ({
-    id: book._id,
-    name: book.name,
-    description: book.description,
-    author: book.author,
-    status: book.status,
-    url: book.url,
-    createdAt: book.updatedAt,
-    updatedAt: book.updatedAt,
-  }));
+  return books.map(bookTransform);
 };
 
 export const bookTransform = (book: Book): BookOutputType => {
