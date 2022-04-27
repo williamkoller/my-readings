542eb7fb3bc23689bc71b5eefcb3952ac7c1aea2
William Koller <williamkoller404@gmail.com>
2022-04-26 23:29:51 -0300

    fix: adjust environment redis

--- src/modules/cache/cache.settings.ts
+++ src/modules/cache/cache.settings.ts
@@ -1,10 +1,8 @@
 import { CacheModule as BaseCacheModule } from '@nestjs/common';
-import * as redisStore from 'cache-manager-redis-store';
 import { CachesRepository } from './repositories/caches.repository';
 
 export const imports = [
   BaseCacheModule.register({
-    store: redisStore,
     url: process.env.REDIS_URL,
     no_ready_check: true,
     isGlobal: true,
