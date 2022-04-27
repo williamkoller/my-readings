9598706c6e10ee009d9f691ea02bd025d89408b3
William Koller <williamkoller404@gmail.com>
2022-04-26 23:42:50 -0300

    fix: adjust environment

--- src/modules/cache/cache.settings.ts
+++ src/modules/cache/cache.settings.ts
@@ -4,10 +4,6 @@ import { CachesRepository } from './repositories/caches.repository';
 export const imports = [
   BaseCacheModule.register({
     url: process.env.REDIS_URL,
-    socket: {
-      tls: true,
-      rejectUnauthorized: false,
-    },
   }),
 ];
 
