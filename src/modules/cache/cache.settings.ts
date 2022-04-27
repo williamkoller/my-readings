46b3d1d9edf7742f2eb20f2760556f43044af999
William Koller <williamkoller404@gmail.com>
2022-04-27 00:54:19 -0300

    feat: add redis url

--- src/modules/cache/cache.settings.ts
+++ src/modules/cache/cache.settings.ts
@@ -4,11 +4,9 @@ import * as redisStore from 'cache-manager-redis-store';
 
 export const imports = [
   BaseCacheModule.register({
-    store: redisStore,
-    socket: {
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT || 12290,
-    },
+    store: redisStore.create({
+      url: process.env.REDIS_URL,
+    }),
   }),
 ];
 
