507f8857375bd6c9d6b83e770db267132e37ca09
William Koller <williamkoller404@gmail.com>
2022-04-27 00:38:49 -0300

    fix: add config redis

--- src/modules/cache/cache.settings.ts
+++ src/modules/cache/cache.settings.ts
@@ -5,8 +5,10 @@ import * as redisStore from 'cache-manager-redis-store';
 export const imports = [
   BaseCacheModule.register({
     store: redisStore,
-    url: process.env.REDIS_URL,
-    isGlobal: true,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    },
   }),
 ];
 
