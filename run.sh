a3c585e64bd6535e0b1040d77f0f05eb4a09233e
William Koller <williamkoller404@gmail.com>
2022-05-11 13:47:33 -0300

    chore: add docker-compose-prod

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/my-readings
 docker-compose build --no-cache
-docker-compose --env-file .env.aws up -d
+docker-compose --env-file .env.aws -f docker-compose-prod.yaml up -d
