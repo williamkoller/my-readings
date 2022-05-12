b2625a8268021264609ae1f4bece082c4bfa46ea
William Koller <williamkoller404@gmail.com>
2022-05-12 09:52:03 -0300

    chore: set new port

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   app.enableCors();
 
   const config = app.get<ConfigService>(ConfigService);
-  const port = config.get<string>('port') || 3000;
+  const port = config.get<string>('port');
   const nodeEnv = config.get<string>('nodeEnv');
 
   await app.listen(port, () =>
